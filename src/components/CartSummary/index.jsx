105ccdc266df04aedb81d2088fdc8df67baead0e
santosxis <kassioxis@icloud.com>
2025-10-10 10:59:33 -0300

    feat: Adiciona funcionalidade de finalizar pedido
    Implementa o fluxo completo para finalizar um pedido no carrinho.
    
    Calcula o valor total dos produtos e a taxa de entrega, e envia os itens para a API de pedidos. Gerencia o feedback ao usuário com toasts e limpa o carrinho após a conclusão do pedido.

--- src/components/CartSummary/index.jsx
+++ src/components/CartSummary/index.jsx
@@ -1,23 +1,74 @@
-import { Button } from "../Button";
-import { Container } from "./styles";
+import { useEffect, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { toast } from 'react-toastify';
+import { useCart } from '../../hooks/CartContext';
+import { api } from '../../services/api';
+import { formatPrice } from '../../utils/formatPrice';
+import { Button } from '../Button';
+import { Container } from './styles';
 
 export function CartSummary() {
+  const [finalPrice, setFinalPrice] = useState(0);
+  const [deliveryTax] = useState(500);
+
+  const navigate = useNavigate();
+
+  const { cartProducts, clearCart } = useCart();
+
+  useEffect(() => {
+    const sumAllProducts = cartProducts.reduce((acc, current) => {
+      return current.price * current.quantity + acc;
+    }, 0);
+    setFinalPrice(sumAllProducts);
+  }, [cartProducts]);
+
+  const submitOrder = async () => {
+    const products = cartProducts.map((product) => ({
+      id: product.id,
+      quantity: product.quantity,
+    }));
+    try {
+      const { status } = await api.post(
+        '/orders',
+        { products },
+        {
+          validateStatus: () => true,
+        },
+      );
+
+      if (status === 201 || status === 200) {
+        setTimeout(() => {
+          clearCart();
+          navigate('/');
+        }, 2000);
+        toast.success('Pedido realizado com sucesso!');
+      } else if (status === 400) {
+        toast.error('⚙️ Falha no sistema! Tente novamente.');
+      } else {
+        throw new Error();
+      }
+    } catch (error) {
+      toast.error('⚙️ Falha no sistema! Tente novamente.');
+    }
+
+  };
+
   return (
     <div>
       <Container>
         <div className="container-top">
           <h2 className="title">Resumo do pedido</h2>
           <p className="itens">Itens</p>
-          <p className="itens-price">R$ 0,00</p>
+          <p className="itens-price">{formatPrice(finalPrice)}</p>
           <p className="delivery-tax">Taxa de entrega</p>
-          <p className="delivery-tax-price">R$ 5,00</p>
+          <p className="delivery-tax-price">{formatPrice(deliveryTax)}</p>
         </div>
         <div className="container-bottom">
-          <p className="">Total</p>
-          <p className="" >R$ 0,00</p>
+          <p>Total</p>
+          <p>{formatPrice(finalPrice + deliveryTax)}</p>
         </div>
       </Container>
-      <Button>Finalizar compra</Button>
+      <Button onClick={submitOrder}>Finalizar compra</Button>
     </div>
   );
 }
