b1e13d0c7e77c27650d147fc31d99635a7c1c6f5
santosxis <kassioxis@icloud.com>
2025-10-12 15:52:56 -0300

    feat: Adiciona tema padrão de cores
    Define um conjunto de cores e fontes para serem usadas consistentemente em toda a aplicação, facilitando a gestão de estilos visuais.

---
+++ src/styles/themes/standard.js
@@ -0,0 +1,22 @@
+export const standardTheme = {
+    purple: '#9758a6',
+    darkPurple: '#5c2669',
+    secondDarkPurple: '#6f357c',
+    orange: '#ff8c05',
+    mainBlack: '#1f1f1f',
+    black: '#363636',
+    secoundBlack: '#484848',
+    white: '#fff',
+    secoundWhite: '#f0f0f0',
+    darkWhite: '#f4f4f4',
+    lightGray: '#cdcdcd',
+    darkGray: '#625e5e',
+    red: '#ff3305',
+    darkRed: '#cf3057',
+    green: '#61a120',
+    roadRageFont: '"Road Rage", sansSerif',
+    poppinsFont: '"Poppins", sansSerif'
+
+    
+
+}
