0b4a02f0ee65de2a1ea36e2928978eda0896544e
santosxis <kassioxis@icloud.com>
2025-10-14 22:26:59 -0300

    feat: Implementa barra lateral de navegação admin
    Implementa a barra lateral de navegação para o painel administrativo.
    Transforma o componente SidNavAdmin de um placeholder em uma interface funcional.
    Adiciona links dinâmicos para Pedidos, Produtos e Adicionar Produto, gerenciamento de logout e estilização completa com styled-components e react-router-dom.
    Integra ícones das bibliotecas Lucide e Phosphor.

--- src/components/SidNavAdmin/index.jsx
+++ src/components/SidNavAdmin/index.jsx
@@ -1,9 +1,29 @@
+import { SignOutIcon } from '@phosphor-icons/react/dist/ssr';
+import Logo from '../../assets/Logo.svg';
+import { useUser } from '../../hooks/UserContext'
+import { navLinks } from './navLinks';
+import { Container, Footer, NavLink, NavLinkContainer } from './styles';
+
 export function SidNavAdmin() {
+  const { logout } = useUser();
+
   return (
-    <div>
-        <h3>
-            Menu
-        </h3>
-    </div>
-  )
+    <Container>
+      <img src={Logo} alt="Hamburguer Logo DevBurger " />
+      <NavLinkContainer>
+        {navLinks.map((navLink) => (
+          <NavLink key={navLink.id} to={navLink.path}>
+            {navLink.icon}
+            <span>{navLink.label}</span>
+          </NavLink>
+        ))}
+      </NavLinkContainer>
+      <Footer>
+        <NavLink to="/login" onClick={logout}>
+          <SignOutIcon fill="#fff" size={32} />
+          <p>Sair</p>
+        </NavLink>
+      </Footer>
+    </Container>
+  );
 }
--- src/components/SidNavAdmin/styles.js
+++ src/components/SidNavAdmin/styles.js
@@ -1,1 +1,52 @@
-import styled from "styled-components";
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+export const Container = styled.nav`
+  display: flex;
+  align-items: center;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: #363636;
+
+  img {
+    width: 60%;
+    margin: 40px 0;
+   /*  margin: 60px auto; */
+  }
+`;
+
+export const NavLinkContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+`;
+
+export const NavLink = styled(Link)`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  padding: 12px 20px;
+  color: #ffffff;
+  text-decoration: none;
+  font-weight: 500;
+  transition: background-color 0.3s ease, transform 0.2s ease;
+  border-radius: 10px;
+
+  span {
+    font-size: 1rem;
+  }
+
+  &:hover {
+    background-color: #9758a6;
+    transform: scale(1.03);
+  }
+`;
+
+export const Footer = styled.footer`
+  margin-top: auto;
+  padding: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
